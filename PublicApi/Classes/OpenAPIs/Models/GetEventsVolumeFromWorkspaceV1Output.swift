1bc32b21ef97b319af9e672fcae61e22bcc3656f
APIs and Common Services team <team-apics@twilio.com>
2023-02-02 18:45:34 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@06cc30e.

--- PublicApi/Classes/OpenAPIs/Models/GetEventsVolumeFromWorkspaceV1Output.swift
+++ PublicApi/Classes/OpenAPIs/Models/GetEventsVolumeFromWorkspaceV1Output.swift
@@ -13,16 +13,23 @@ import AnyCodable
 /** GetEventsVolumeFromWorkspaceV1Output represents the results given the input query. */
 public struct GetEventsVolumeFromWorkspaceV1Output: Codable, JSONEncodable, Hashable {
 
+    /** Observability event volume path. */
+    public var path: String
+    public var query: Query
     /** The resultant list of series broken down by the dimensions requested over the time frame requested and ordered by the total count of events in all series. Note: The limit of entries returned is 5000. */
     public var result: [SourceEventVolumeV1]
     public var pagination: Pagination?
 
-    public init(result: [SourceEventVolumeV1], pagination: Pagination? = nil) {
+    public init(path: String, query: Query, result: [SourceEventVolumeV1], pagination: Pagination? = nil) {
+        self.path = path
+        self.query = query
         self.result = result
         self.pagination = pagination
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
+        case path
+        case query
         case result
         case pagination
     }
@@ -31,6 +38,8 @@ public struct GetEventsVolumeFromWorkspaceV1Output: Codable, JSONEncodable, Hash
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(path, forKey: .path)
+        try container.encode(query, forKey: .query)
         try container.encode(result, forKey: .result)
         try container.encodeIfPresent(pagination, forKey: .pagination)
     }
