e1f6b2cf52bda4ef5d3517e63adfcdd88dfc1b64
APIs and Common Services team <team-apics@twilio.com>
2024-03-13 21:09:00 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@67ea11f6.

--- PublicApi/Classes/OpenAPIs/Models/UpdateAudienceForSpaceInput.swift
+++ PublicApi/Classes/OpenAPIs/Models/UpdateAudienceForSpaceInput.swift
@@ -14,9 +14,9 @@ import AnyCodable
 public struct UpdateAudienceForSpaceInput: Codable, JSONEncodable, Hashable {
 
     /** Enabled/disabled status for the audience. */
-    public var enabled: Bool
+    public var enabled: Bool?
 
-    public init(enabled: Bool) {
+    public init(enabled: Bool? = nil) {
         self.enabled = enabled
     }
 
@@ -28,7 +28,7 @@ public struct UpdateAudienceForSpaceInput: Codable, JSONEncodable, Hashable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(enabled, forKey: .enabled)
+        try container.encodeIfPresent(enabled, forKey: .enabled)
     }
 }
 
