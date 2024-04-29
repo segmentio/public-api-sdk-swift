bfc66ae39ff64350e78be5b9c142b2550b9fc98b
APIs and Common Services team <team-apics@twilio.com>
2024-04-29 19:48:30 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@f42fcef6.

--- PublicApi/Classes/OpenAPIs/Models/UpdateAudienceForSpaceInput.swift
+++ PublicApi/Classes/OpenAPIs/Models/UpdateAudienceForSpaceInput.swift
@@ -19,9 +19,9 @@ public struct UpdateAudienceForSpaceInput: Codable, JSONEncodable, Hashable {
     public var name: String?
     /** The description of the computation. */
     public var description: String?
-    public var definition: Definition3?
+    public var definition: Definition1?
 
-    public init(enabled: Bool? = nil, name: String? = nil, description: String? = nil, definition: Definition3? = nil) {
+    public init(enabled: Bool? = nil, name: String? = nil, description: String? = nil, definition: Definition1? = nil) {
         self.enabled = enabled
         self.name = name
         self.description = description
