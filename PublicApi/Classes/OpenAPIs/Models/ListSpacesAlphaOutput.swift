7bf5cfe1285fa763e1e287dcbfe336036fbe4c52
APIs and Common Services team <team-apics@twilio.com>
2024-05-31 19:33:50 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@043d4b9e.

--- PublicApi/Classes/OpenAPIs/Models/ListSpacesAlphaOutput.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListSpacesAlphaOutput.swift
@@ -15,9 +15,9 @@ public struct ListSpacesAlphaOutput: Codable, JSONEncodable, Hashable {
 
     /** A list of spaces. */
     public var spaces: [Space]
-    public var pagination: Pagination2
+    public var pagination: Pagination
 
-    public init(spaces: [Space], pagination: Pagination2) {
+    public init(spaces: [Space], pagination: Pagination) {
         self.spaces = spaces
         self.pagination = pagination
     }
