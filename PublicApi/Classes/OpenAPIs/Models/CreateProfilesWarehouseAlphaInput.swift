eb00813621c0e9b8730ef73cd03c18c1b2e30476
APIs and Common Services team <team-apics@twilio.com>
2024-06-12 21:04:00 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@cb9f6139.

--- PublicApi/Classes/OpenAPIs/Models/CreateProfilesWarehouseAlphaInput.swift
+++ PublicApi/Classes/OpenAPIs/Models/CreateProfilesWarehouseAlphaInput.swift
@@ -21,7 +21,7 @@ public struct CreateProfilesWarehouseAlphaInput: Codable, JSONEncodable, Hashabl
     public var enabled: Bool?
     /** A key-value object that contains instance-specific settings for a Warehouse.  Different kinds of Warehouses require different settings. The required and optional settings for a Warehouse are described in the `options` object of the associated Warehouse metadata.  You can find the full list of Warehouse metadata and related settings information in the `/catalog/warehouses` endpoint. */
     public var settings: JSON?
-    /** The custom schema name that Segment uses on the Warehouse side. The space slug value is default otherwise. */
+    /** The custom schema name that Segment uses on the Warehouse side. The space slug value is default otherwise. The schema name cannot be an existing schema name in the Warehouse. To use an existing schema name, please create a profiles Warehouse through the Segment app UI. */
     public var schemaName: String?
 
     public init(metadataId: String, name: String? = nil, enabled: Bool? = nil, settings: JSON?, schemaName: String? = nil) {
