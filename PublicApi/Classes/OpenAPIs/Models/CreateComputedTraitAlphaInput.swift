87ee9b2218603da1ee440390b05f7237a6e907ae
APIs and Common Services team <team-apics@twilio.com>
2024-09-10 00:09:56 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@3363268f.

--- PublicApi/Classes/OpenAPIs/Models/CreateComputedTraitAlphaInput.swift
+++ PublicApi/Classes/OpenAPIs/Models/CreateComputedTraitAlphaInput.swift
@@ -19,10 +19,10 @@ public struct CreateComputedTraitAlphaInput: Codable, JSONEncodable, Hashable {
     public var enabled: Bool?
     /** The description of the computation. */
     public var description: String?
-    public var definition: Definition3
-    public var options: Options2?
+    public var definition: Definition
+    public var options: Options?
 
-    public init(name: String, enabled: Bool? = nil, description: String? = nil, definition: Definition3, options: Options2? = nil) {
+    public init(name: String, enabled: Bool? = nil, description: String? = nil, definition: Definition, options: Options? = nil) {
         self.name = name
         self.enabled = enabled
         self.description = description
