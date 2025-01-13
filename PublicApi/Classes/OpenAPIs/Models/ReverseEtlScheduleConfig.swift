3be753cd74158cd80a867141b491595da8f18262
APIs and Common Services team <team-apics@twilio.com>
2025-01-13 19:58:18 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@7b03d4b7.

--- PublicApi/Classes/OpenAPIs/Models/ReverseEtlScheduleConfig.swift
+++ PublicApi/Classes/OpenAPIs/Models/ReverseEtlScheduleConfig.swift
@@ -22,13 +22,19 @@ public struct ReverseEtlScheduleConfig: Codable, JSONEncodable, Hashable {
     public var timezone: String
     /** 5 field cron string expression. The cron expression must be larger than 15 minutes. */
     public var spec: String
+    /** The dbt Cloud job used to trigger a sync for a Reverse ETL Connection. */
+    public var jobId: String
+    /** The dbt Cloud account where the job belongs to. */
+    public var accountId: String
 
-    public init(interval: String, days: [Double], hours: [Double], timezone: String, spec: String) {
+    public init(interval: String, days: [Double], hours: [Double], timezone: String, spec: String, jobId: String, accountId: String) {
         self.interval = interval
         self.days = days
         self.hours = hours
         self.timezone = timezone
         self.spec = spec
+        self.jobId = jobId
+        self.accountId = accountId
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
@@ -37,6 +43,8 @@ public struct ReverseEtlScheduleConfig: Codable, JSONEncodable, Hashable {
         case hours
         case timezone
         case spec
+        case jobId
+        case accountId
     }
 
     // Encodable protocol methods
@@ -48,6 +56,8 @@ public struct ReverseEtlScheduleConfig: Codable, JSONEncodable, Hashable {
         try container.encode(hours, forKey: .hours)
         try container.encode(timezone, forKey: .timezone)
         try container.encode(spec, forKey: .spec)
+        try container.encode(jobId, forKey: .jobId)
+        try container.encode(accountId, forKey: .accountId)
     }
 }
 
