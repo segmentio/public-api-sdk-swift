88921a3b661d04c56d8f9c3571091c39943b63d8
APIs and Common Services team <team-apics@twilio.com>
2025-04-09 00:10:21 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@1722e5a1.

--- PublicApi/Classes/OpenAPIs/Models/Definition5.swift
+++ PublicApi/Classes/OpenAPIs/Models/Definition5.swift
@@ -10,34 +10,34 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Represents the Compute Query Language definition of the computation and type of computation. Note: The definition for an Audience updated using the API is not editable through the Segment App. */
+/** Query language definition and type. */
 public struct Definition5: Codable, JSONEncodable, Hashable {
 
     public enum ModelType: String, Codable, CaseIterable {
         case accounts = "ACCOUNTS"
         case users = "USERS"
     }
-    /** The underlying data type being segmented for this audience.  Possible values: users, accounts. */
-    public var type: ModelType
-    /** The query language string defining the audience segmentation criteria.  For guidance on using the query language, see the [Segment documentation site](https://segment.com/docs/api/public-api/query-language). */
+    /** The query language string defining the computed trait aggregation criteria. For guidance on using the query language, see the [Segment documentation site](https://segment.com/docs/api/public-api/query-language). */
     public var query: String
+    /** The underlying data type being aggregated for this computed trait.  Possible values: users, accounts. */
+    public var type: ModelType
 
-    public init(type: ModelType, query: String) {
-        self.type = type
+    public init(query: String, type: ModelType) {
         self.query = query
+        self.type = type
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
-        case type
         case query
+        case type
     }
 
     // Encodable protocol methods
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(type, forKey: .type)
         try container.encode(query, forKey: .query)
+        try container.encode(type, forKey: .type)
     }
 }
 
