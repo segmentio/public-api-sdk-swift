911fee8e27e56eb0fabe99002bcfc7368ee38523
APIs and Common Services team <team-apics@twilio.com>
2025-04-30 00:10:37 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@17d47d85.

--- PublicApi/Classes/OpenAPIs/Models/Options1.swift
+++ PublicApi/Classes/OpenAPIs/Models/Options1.swift
@@ -10,12 +10,12 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Options which should be applied when segmenting traits. */
+/** Options which should be applied when segmenting audiences. */
 public struct Options1: Codable, JSONEncodable, Hashable {
 
-    /** Determines whether data prior to the computed trait being created is included when determining the computed trait value. Note that including historical data may be needed in order to properly handle the definition specified. In these cases, Segment will automatically handle including historical data and the response will return the includeHistoricalData parameter as true. */
+    /** Determines whether data prior to the audience being created is included when determining audience membership. Note that including historical data may be needed in order to properly handle the definition specified. In these cases, Segment will automatically handle including historical data and the response will return the includeHistoricalData parameter as true. */
     public var includeHistoricalData: Bool?
-    /** Determines whether anonymous users should be included when determining the computed trait value. */
+    /** Determines whether anonymous users should be included when determining audience membership. */
     public var includeAnonymousUsers: Bool?
 
     public init(includeHistoricalData: Bool? = nil, includeAnonymousUsers: Bool? = nil) {
