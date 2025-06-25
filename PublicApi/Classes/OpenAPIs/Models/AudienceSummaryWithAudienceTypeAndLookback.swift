2050d2408aef8ca9e9bee3c208057fc4518767d8
APIs and Common Services team <team-apics@twilio.com>
2025-06-25 18:19:50 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@e9968715.

--- PublicApi/Classes/OpenAPIs/Models/AudienceSummaryWithAudienceTypeAndLookback.swift
+++ PublicApi/Classes/OpenAPIs/Models/AudienceSummaryWithAudienceTypeAndLookback.swift
@@ -20,6 +20,7 @@ public struct AudienceSummaryWithAudienceTypeAndLookback: Codable, JSONEncodable
     /** Discriminator denoting the audience's product type. */
     public var audienceType: AudienceType
     public var computeCadence: ComputeCadence
+    public var size: Size1?
     public var options: Options6?
     /** Audience id. */
     public var id: String
@@ -45,9 +46,10 @@ public struct AudienceSummaryWithAudienceTypeAndLookback: Codable, JSONEncodable
     /** Date the audience was last updated. */
     public var updatedAt: String
 
-    public init(audienceType: AudienceType, computeCadence: ComputeCadence, options: Options6? = nil, id: String, spaceId: String, name: String, description: String? = nil, key: String, enabled: Bool, definition: Definition7?, status: String? = nil, createdBy: String, updatedBy: String, createdAt: String, updatedAt: String) {
+    public init(audienceType: AudienceType, computeCadence: ComputeCadence, size: Size1? = nil, options: Options6? = nil, id: String, spaceId: String, name: String, description: String? = nil, key: String, enabled: Bool, definition: Definition7?, status: String? = nil, createdBy: String, updatedBy: String, createdAt: String, updatedAt: String) {
         self.audienceType = audienceType
         self.computeCadence = computeCadence
+        self.size = size
         self.options = options
         self.id = id
         self.spaceId = spaceId
@@ -66,6 +68,7 @@ public struct AudienceSummaryWithAudienceTypeAndLookback: Codable, JSONEncodable
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case audienceType
         case computeCadence
+        case size
         case options
         case id
         case spaceId
@@ -87,6 +90,7 @@ public struct AudienceSummaryWithAudienceTypeAndLookback: Codable, JSONEncodable
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(audienceType, forKey: .audienceType)
         try container.encode(computeCadence, forKey: .computeCadence)
+        try container.encodeIfPresent(size, forKey: .size)
         try container.encodeIfPresent(options, forKey: .options)
         try container.encode(id, forKey: .id)
         try container.encode(spaceId, forKey: .spaceId)
