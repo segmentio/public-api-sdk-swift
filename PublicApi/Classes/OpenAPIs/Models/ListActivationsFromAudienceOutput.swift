939b02f60ad87be8295bbe2d94fd37c3304fec73
APIs and Common Services team <team-apics@twilio.com>
2025-08-28 00:10:37 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@79bf93fc.

--- PublicApi/Classes/OpenAPIs/Models/ListActivationsFromAudienceOutput.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListActivationsFromAudienceOutput.swift
@@ -15,9 +15,9 @@ public struct ListActivationsFromAudienceOutput: Codable, JSONEncodable, Hashabl
 
     /** A list of activation results. */
     public var activations: [ActivationSummaryOutput]
-    public var pagination: Pagination3?
+    public var pagination: Pagination1?
 
-    public init(activations: [ActivationSummaryOutput], pagination: Pagination3? = nil) {
+    public init(activations: [ActivationSummaryOutput], pagination: Pagination1? = nil) {
         self.activations = activations
         self.pagination = pagination
     }
