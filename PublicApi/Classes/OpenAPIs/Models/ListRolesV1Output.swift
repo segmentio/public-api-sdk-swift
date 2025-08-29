01cda6b5580eb06225f9d11fdf7eb447e33306df
APIs and Common Services team <team-apics@twilio.com>
2025-08-29 00:10:31 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@0ed000c8.

--- PublicApi/Classes/OpenAPIs/Models/ListRolesV1Output.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListRolesV1Output.swift
@@ -15,9 +15,9 @@ public struct ListRolesV1Output: Codable, JSONEncodable, Hashable {
 
     /** The list of roles. */
     public var roles: [RoleV1]
-    public var pagination: Pagination
+    public var pagination: Pagination3
 
-    public init(roles: [RoleV1], pagination: Pagination) {
+    public init(roles: [RoleV1], pagination: Pagination3) {
         self.roles = roles
         self.pagination = pagination
     }
