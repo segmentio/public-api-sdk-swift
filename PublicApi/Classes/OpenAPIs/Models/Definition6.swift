772fc75fdeeeeedaa2b93c3ff4da9fdeb431b15c
APIs and Common Services team <team-apics@twilio.com>
2025-09-15 20:27:28 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@0b6cdacd.

--- PublicApi/Classes/OpenAPIs/Models/Definition6.swift
+++ PublicApi/Classes/OpenAPIs/Models/Definition6.swift
@@ -10,7 +10,7 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Query language definition and type. */
+/** Query language definition. */
 public struct Definition6: Codable, JSONEncodable, Hashable {
 
     public enum ModelType: String, Codable, CaseIterable {
