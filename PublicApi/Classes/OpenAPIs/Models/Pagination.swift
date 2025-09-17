b0aaf88bdcea21a56ac6d940d11ef533dda67e7e
APIs and Common Services team <team-apics@twilio.com>
2025-09-17 04:21:05 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@da0d545f.

--- PublicApi/Classes/OpenAPIs/Models/Pagination.swift
+++ PublicApi/Classes/OpenAPIs/Models/Pagination.swift
@@ -10,7 +10,7 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Information about the pagination of this response.  [See pagination](https://docs.segmentapis.com/tag/Pagination/#section/Pagination-parameters) for more info. */
+/** An optional pagination object that is present when pagination input is present. */
 public struct Pagination: Codable, JSONEncodable, Hashable {
 
     /** The current cursor within a collection.  Consumers of the API must treat this value as opaque. */
