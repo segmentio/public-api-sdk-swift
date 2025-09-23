0370eed2fe38553ff746f61cd19e432c6e00524c
APIs and Common Services team <team-apics@twilio.com>
2025-09-23 22:35:41 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@5e667899.

--- PublicApi/Classes/OpenAPIs/Models/ListUsersFromUserGroupV1Output.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListUsersFromUserGroupV1Output.swift
@@ -15,9 +15,9 @@ public struct ListUsersFromUserGroupV1Output: Codable, JSONEncodable, Hashable {
 
     /** The users of the user group. */
     public var users: [MinimalUserV1]
-    public var pagination: Pagination1
+    public var pagination: Pagination
 
-    public init(users: [MinimalUserV1], pagination: Pagination1) {
+    public init(users: [MinimalUserV1], pagination: Pagination) {
         self.users = users
         self.pagination = pagination
     }
