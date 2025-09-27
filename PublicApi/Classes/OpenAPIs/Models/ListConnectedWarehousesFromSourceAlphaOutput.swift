7101765c94c79b9164775944ad288e8451cc654f
APIs and Common Services team <team-apics@twilio.com>
2025-09-27 00:44:20 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@289c4a65.

--- PublicApi/Classes/OpenAPIs/Models/ListConnectedWarehousesFromSourceAlphaOutput.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListConnectedWarehousesFromSourceAlphaOutput.swift
@@ -15,9 +15,9 @@ public struct ListConnectedWarehousesFromSourceAlphaOutput: Codable, JSONEncodab
 
     /** A list that contains the Warehouses connected to the Source. */
     public var warehouses: [WarehouseV1]
-    public var pagination: Pagination3
+    public var pagination: Pagination1
 
-    public init(warehouses: [WarehouseV1], pagination: Pagination3) {
+    public init(warehouses: [WarehouseV1], pagination: Pagination1) {
         self.warehouses = warehouses
         self.pagination = pagination
     }
