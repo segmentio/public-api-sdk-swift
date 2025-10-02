3f61b889f1862afbb79e7a2d34f7b44c1eb1a332
APIs and Common Services team <team-apics@twilio.com>
2025-10-02 00:10:48 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@625487ea.

--- PublicApi/Classes/OpenAPIs/Models/ListTransformationsBetaOutput.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListTransformationsBetaOutput.swift
@@ -15,9 +15,9 @@ public struct ListTransformationsBetaOutput: Codable, JSONEncodable, Hashable {
 
     /** A paginated list of Transformations. */
     public var transformations: [TransformationBeta]
-    public var pagination: Pagination1
+    public var pagination: Pagination3
 
-    public init(transformations: [TransformationBeta], pagination: Pagination1) {
+    public init(transformations: [TransformationBeta], pagination: Pagination3) {
         self.transformations = transformations
         self.pagination = pagination
     }
