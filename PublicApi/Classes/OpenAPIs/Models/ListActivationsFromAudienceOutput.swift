15bb5dd7599188dd6918c515d91ae9b6d9b4903b
APIs and Common Services team <team-apics@twilio.com>
2025-10-08 00:11:17 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@baf62695.

--- PublicApi/Classes/OpenAPIs/Models/ListActivationsFromAudienceOutput.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListActivationsFromAudienceOutput.swift
@@ -15,9 +15,9 @@ public struct ListActivationsFromAudienceOutput: Codable, JSONEncodable, Hashabl
 
     /** A list of activation results. */
     public var activations: [ActivationOutput]
-    public var pagination: Pagination3?
+    public var pagination: Pagination2?
 
-    public init(activations: [ActivationOutput], pagination: Pagination3? = nil) {
+    public init(activations: [ActivationOutput], pagination: Pagination2? = nil) {
         self.activations = activations
         self.pagination = pagination
     }
--- PublicApi/Classes/OpenAPIs/Models/Pagination2.swift
+++ PublicApi/Classes/OpenAPIs/Models/Pagination2.swift
@@ -10,7 +10,7 @@ import Foundation
 import AnyCodable
 #endif
 
-/** The result object used to specify the page cursor and count. */
+/** An optional pagination object that is present when pagination input is present. */
 public struct Pagination2: Codable, JSONEncodable, Hashable {
 
     /** The current cursor within a collection.  Consumers of the API must treat this value as opaque. */
