743901b9d4701c74f9df016dce4c2add06703fef
APIs and Common Services team <team-apics@twilio.com>
2025-10-16 13:25:50 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@63c805bd.

--- PublicApi/Classes/OpenAPIs/Models/Options5.swift
+++ PublicApi/Classes/OpenAPIs/Models/Options5.swift
@@ -10,35 +10,35 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Options which should be applied when segmenting audiences. */
+/** Options which should be applied when aggregating computed traits. */
 public struct Options5: Codable, JSONEncodable, Hashable {
 
-    /** The set of profile external identifiers being used to determine audience membership. Profiles will only be considered for audience membership if the profile has at least one external id whose key matches a value in this set. */
-    public var filterByExternalIds: [String]
-    /** Determines whether data prior to the audience being created is included when determining audience membership. Note that including historical data may be needed in order to properly handle the definition specified. In these cases, Segment will automatically handle including historical data and the response will return the includeHistoricalData parameter as true. */
+    /** Determines whether data prior to the computed trait being created is included when determining the computed trait value. Note that including historical data may be needed in order to properly handle the definition specified. In these cases, Segment will automatically handle including historical data and the response will return the includeHistoricalData parameter as true. */
     public var includeHistoricalData: Bool?
-    /** If specified and positive, the value of this field indicates the number of days, specified from the date the audience was created, that event data will be included from when determining audience membership. If unspecified, defer to the value of `includeHistoricalData` to determine whether historical data is either entirely included or entirely excluded when determining audience membership. */
-    public var backfillEventDataDays: Double?
+    /** Determines whether anonymous users should be included when determining the computed trait value. */
+    public var includeAnonymousUsers: Bool?
+    /** If specified, the value of this field indicates the number of days, specified from the date the audience was created, that event data will be included from when determining audience membership. If unspecified, defer to the value of `includeHistoricalData` to determine whether historical data is either entirely included or entirely excluded when determining audience membership. */
+    public var backfillDurationDays: Double?
 
-    public init(filterByExternalIds: [String], includeHistoricalData: Bool? = nil, backfillEventDataDays: Double? = nil) {
-        self.filterByExternalIds = filterByExternalIds
+    public init(includeHistoricalData: Bool? = nil, includeAnonymousUsers: Bool? = nil, backfillDurationDays: Double? = nil) {
         self.includeHistoricalData = includeHistoricalData
-        self.backfillEventDataDays = backfillEventDataDays
+        self.includeAnonymousUsers = includeAnonymousUsers
+        self.backfillDurationDays = backfillDurationDays
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
-        case filterByExternalIds
         case includeHistoricalData
-        case backfillEventDataDays
+        case includeAnonymousUsers
+        case backfillDurationDays
     }
 
     // Encodable protocol methods
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(filterByExternalIds, forKey: .filterByExternalIds)
         try container.encodeIfPresent(includeHistoricalData, forKey: .includeHistoricalData)
-        try container.encodeIfPresent(backfillEventDataDays, forKey: .backfillEventDataDays)
+        try container.encodeIfPresent(includeAnonymousUsers, forKey: .includeAnonymousUsers)
+        try container.encodeIfPresent(backfillDurationDays, forKey: .backfillDurationDays)
     }
 }
 
