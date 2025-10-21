07b14511a20d55e072dc8eeae4f0d8fdbedcb9b8
APIs and Common Services team <team-apics@twilio.com>
2025-10-21 18:32:20 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@cd04343d.

--- PublicApi/Classes/OpenAPIs/Models/Definition.swift
+++ PublicApi/Classes/OpenAPIs/Models/Definition.swift
@@ -10,25 +10,21 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Represents the Compute Query Language definition of the computation and type of computation. */
+/** Query language definition and type. */
 public struct Definition: Codable, JSONEncodable, Hashable {
 
-    public enum ModelType: String, Codable, CaseIterable {
-        case accounts = "ACCOUNTS"
-        case users = "USERS"
-    }
-    /** The underlying data type being aggregated for this computed trait.  Possible values: users, accounts. */
-    public var type: ModelType
-    /** The query language string defining the computed trait aggregation criteria. For guidance on using the query language, see the [Segment documentation site](https://segment.com/docs/api/public-api/query-language). */
+    /** The target entity relationship slug, required in a linked audience, default to profile if not specified. */
+    public var targetEntity: String?
+    /** The query language string defining the audience segmentation criteria.  For guidance on using the query language, see the [Segment documentation site](https://segment.com/docs/api/public-api/query-language). */
     public var query: String
 
-    public init(type: ModelType, query: String) {
-        self.type = type
+    public init(targetEntity: String? = nil, query: String) {
+        self.targetEntity = targetEntity
         self.query = query
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
-        case type
+        case targetEntity
         case query
     }
 
@@ -36,7 +32,7 @@ public struct Definition: Codable, JSONEncodable, Hashable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(type, forKey: .type)
+        try container.encodeIfPresent(targetEntity, forKey: .targetEntity)
         try container.encode(query, forKey: .query)
     }
 }
