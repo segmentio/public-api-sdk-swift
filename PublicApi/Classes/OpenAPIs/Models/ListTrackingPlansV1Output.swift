d565536e1536fce75540b9cd6e11313029d25284
APIs and Common Services team <team-apics@twilio.com>
2025-11-18 00:10:32 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@dcbdc053.

--- PublicApi/Classes/OpenAPIs/Models/ListTrackingPlansV1Output.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListTrackingPlansV1Output.swift
@@ -15,9 +15,9 @@ public struct ListTrackingPlansV1Output: Codable, JSONEncodable, Hashable {
 
     /** A paginated list of Tracking Plans. */
     public var trackingPlans: [TrackingPlanV1]
-    public var pagination: Pagination2
+    public var pagination: Pagination3
 
-    public init(trackingPlans: [TrackingPlanV1], pagination: Pagination2) {
+    public init(trackingPlans: [TrackingPlanV1], pagination: Pagination3) {
         self.trackingPlans = trackingPlans
         self.pagination = pagination
     }
