0f70cc7d2ff3a1509988b46274920d48ed18d5db
APIs and Common Services team <team-apics@twilio.com>
2026-02-04 20:22:12 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@d97d089c.

--- PublicApi/Classes/OpenAPIs/APIs/AudiencesAPI.swift
+++ PublicApi/Classes/OpenAPIs/APIs/AudiencesAPI.swift
@@ -125,13 +125,13 @@ open class AudiencesAPI {
      Create Audience Preview
      
      - parameter spaceId: (path)  
-     - parameter createAudiencePreviewBetaInput: (body)  
+     - parameter createAudiencePreviewInput: (body)  
      - parameter apiResponseQueue: The queue on which api response is dispatched.
      - parameter completion: completion handler to receive the data and the error objects
      */
     @discardableResult
-    open class func createAudiencePreview(spaceId: String, createAudiencePreviewBetaInput: CreateAudiencePreviewBetaInput, apiResponseQueue: DispatchQueue = PublicApiAPI.apiResponseQueue, completion: @escaping ((_ data: CreateAudiencePreview200Response?, _ error: Error?) -> Void)) -> RequestTask {
-        return createAudiencePreviewWithRequestBuilder(spaceId: spaceId, createAudiencePreviewBetaInput: createAudiencePreviewBetaInput).execute(apiResponseQueue) { result in
+    open class func createAudiencePreview(spaceId: String, createAudiencePreviewInput: CreateAudiencePreviewInput, apiResponseQueue: DispatchQueue = PublicApiAPI.apiResponseQueue, completion: @escaping ((_ data: CreateAudiencePreview200Response?, _ error: Error?) -> Void)) -> RequestTask {
+        return createAudiencePreviewWithRequestBuilder(spaceId: spaceId, createAudiencePreviewInput: createAudiencePreviewInput).execute(apiResponseQueue) { result in
             switch result {
             case let .success(response):
                 completion(response.body, nil)
@@ -144,21 +144,21 @@ open class AudiencesAPI {
     /**
      Create Audience Preview
      - POST /spaces/{spaceId}/audiences/previews
-     - Previews Audience.  • This endpoint is in **Beta** testing.  Please submit any feedback by sending an email to friends@segment.com.   • In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.  • When called, this endpoint may generate the `Audience Preview Created` event in the [audit trail](/tag/Audit-Trail).   The rate limit for this endpoint is 5 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information. This endpoint also has a rate limit of 700 requests per month per spaceId, which is lower than the default due to access pattern restrictions.
+     - Previews Audience.  • In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.  • When called, this endpoint may generate the `Audience Preview Created` event in the [audit trail](/tag/Audit-Trail).   The rate limit for this endpoint is 5 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information. This endpoint also has a rate limit of 700 requests per month per spaceId, which is lower than the default due to access pattern restrictions.
      - BASIC:
        - type: http
        - name: token
      - parameter spaceId: (path)  
-     - parameter createAudiencePreviewBetaInput: (body)  
+     - parameter createAudiencePreviewInput: (body)  
      - returns: RequestBuilder<CreateAudiencePreview200Response> 
      */
-    open class func createAudiencePreviewWithRequestBuilder(spaceId: String, createAudiencePreviewBetaInput: CreateAudiencePreviewBetaInput) -> RequestBuilder<CreateAudiencePreview200Response> {
+    open class func createAudiencePreviewWithRequestBuilder(spaceId: String, createAudiencePreviewInput: CreateAudiencePreviewInput) -> RequestBuilder<CreateAudiencePreview200Response> {
         var localVariablePath = "/spaces/{spaceId}/audiences/previews"
         let spaceIdPreEscape = "\(APIHelper.mapValueToPathItem(spaceId))"
         let spaceIdPostEscape = spaceIdPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
         localVariablePath = localVariablePath.replacingOccurrences(of: "{spaceId}", with: spaceIdPostEscape, options: .literal, range: nil)
         let localVariableURLString = PublicApiAPI.basePath + localVariablePath
-        let localVariableParameters = JSONEncodingHelper.encodingParameters(forEncodableObject: createAudiencePreviewBetaInput)
+        let localVariableParameters = JSONEncodingHelper.encodingParameters(forEncodableObject: createAudiencePreviewInput)
 
         let localVariableUrlComponents = URLComponents(string: localVariableURLString)
 
