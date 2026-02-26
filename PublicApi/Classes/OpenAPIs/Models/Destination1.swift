5dcc9569e9eaaa6ce61c6f18bfaf2d799d36b20f
APIs and Common Services team <team-apics@twilio.com>
2026-02-26 11:14:19 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@e8fa5c41.

--- PublicApi/Classes/OpenAPIs/Models/Destination1.swift
+++ PublicApi/Classes/OpenAPIs/Models/Destination1.swift
@@ -10,37 +10,18 @@ import Foundation
 import AnyCodable
 #endif
 
-/** The Destination looked up. */
+/** The Destination that was updated. */
 public struct Destination1: Codable, JSONEncodable, Hashable {
 
-    /** The unique identifier of this instance of a Destination.  Config API note: analogous to `name`. */
+    /** The id of the Destination that was updated. */
     public var id: String
-    /** The name of this instance of a Destination.  Config API note: equal to `displayName`. */
-    public var name: String?
-    /** Whether this instance of a Destination receives data. */
-    public var enabled: Bool
-    public var metadata: Metadata1
-    /** The id of a Source connected to this instance of a Destination.  Config API note: analogous to `parent`. */
-    public var sourceId: String
-    /** The collection of settings associated with a Destination.  Config API note: equal to `config`. */
-    public var settings: [String: AnyCodable]
 
-    public init(id: String, name: String? = nil, enabled: Bool, metadata: Metadata1, sourceId: String, settings: [String: AnyCodable]) {
+    public init(id: String) {
         self.id = id
-        self.name = name
-        self.enabled = enabled
-        self.metadata = metadata
-        self.sourceId = sourceId
-        self.settings = settings
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case id
-        case name
-        case enabled
-        case metadata
-        case sourceId
-        case settings
     }
 
     // Encodable protocol methods
@@ -48,11 +29,6 @@ public struct Destination1: Codable, JSONEncodable, Hashable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
-        try container.encodeIfPresent(name, forKey: .name)
-        try container.encode(enabled, forKey: .enabled)
-        try container.encode(metadata, forKey: .metadata)
-        try container.encode(sourceId, forKey: .sourceId)
-        try container.encode(settings, forKey: .settings)
     }
 }
 
