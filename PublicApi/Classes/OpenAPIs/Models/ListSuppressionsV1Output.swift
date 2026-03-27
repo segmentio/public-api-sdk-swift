804b867a8d051ca1fdd6cf9a8a3256416fe9c5e3
APIs and Common Services team <team-apics@twilio.com>
2026-03-27 00:12:16 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@44f74fe8.

--- PublicApi/Classes/OpenAPIs/Models/ListSuppressionsV1Output.swift
+++ PublicApi/Classes/OpenAPIs/Models/ListSuppressionsV1Output.swift
@@ -15,9 +15,9 @@ public struct ListSuppressionsV1Output: Codable, JSONEncodable, Hashable {
 
     /** An array that lists the suppressions from the Workspace.  Config API note: equal to `attributes`. */
     public var suppressed: [SuppressedInner]
-    public var pagination: Pagination3
+    public var pagination: Pagination2
 
-    public init(suppressed: [SuppressedInner], pagination: Pagination3) {
+    public init(suppressed: [SuppressedInner], pagination: Pagination2) {
         self.suppressed = suppressed
         self.pagination = pagination
     }
