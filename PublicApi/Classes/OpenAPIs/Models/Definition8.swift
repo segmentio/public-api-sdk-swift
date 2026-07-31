1827c017c76bb3f4d585190f24afff6004081766
APIs and Common Services team <team-apics@twilio.com>
2026-07-31 05:11:00 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@5b6529b0.

--- PublicApi/Classes/OpenAPIs/Models/Definition8.swift
+++ PublicApi/Classes/OpenAPIs/Models/Definition8.swift
@@ -10,25 +10,21 @@ import Foundation
 import AnyCodable
 #endif
 
-/** Query language definition and type. Note: The definition for a Computed Trait updated using the API is not editable through the Segment App. */
+/** Definition that was used to calculate the audience preview. */
 public struct Definition8: Codable, JSONEncodable, Hashable {
 
-    public enum ModelType: String, Codable, CaseIterable {
-        case accounts = "ACCOUNTS"
-        case users = "USERS"
-    }
-    /** The underlying data type being aggregated for this computed trait.  Possible values: users, accounts. */
-    public var type: ModelType
-    /** The query language string defining the computed trait aggregation criteria. For guidance on using the query language, see the [Segment documentation site](https://segment.com/docs/api/public-api/query-language). */
+    /** The target entity relationship slug, only applicable for linked audiences. Use ```profile``` when the targeting the profile.  Note that the value defaults to profile if not specified. Also note, that the value will be returned as null if the target entity is removed from the data graph. */
+    public var targetEntity: String?
+    /** The query language string defining the audience segmentation criteria.  For guidance on using the query language, see the [Segment documentation site](https://segment.com/docs/api/public-api/query-language). */
     public var query: String
 
-    public init(type: ModelType, query: String) {
-        self.type = type
+    public init(targetEntity: String? = nil, query: String) {
+        self.targetEntity = targetEntity
         self.query = query
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
-        case type
+        case targetEntity
         case query
     }
 
@@ -36,7 +32,7 @@ public struct Definition8: Codable, JSONEncodable, Hashable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(type, forKey: .type)
+        try container.encodeIfPresent(targetEntity, forKey: .targetEntity)
         try container.encode(query, forKey: .query)
     }
 }
